d74db9d9482cda01c68ace14a8413386dfd05804
raktim <raktimdas09@gmail.com>
2025-08-14 23:50:20 +0530

    string updates

--- src/Random_String.java
+++ src/Random_String.java
@@ -5,13 +5,13 @@ public class Random_String {
     public static void main(String[] args) {
         Random random = new Random();
 
-        String[] choice = {"Rock", "Paper", "Scissors", "Mobile", "Notepad"};
+        String[] choices = {"Rock", "Paper", "Scissors", "Mobile", "Notepad"};
 
         // Generate a random index between 0 and 4 (for 5 items in the array)
         int computerChoiceIndex = random.nextInt(5);
 
         // Get the corresponding choice from the array
-        String computerChoice = choice[computerChoiceIndex];
+        String computerChoice = choices[computerChoiceIndex];
 
         // Output the computer's random choice
         System.out.println(computerChoice);
