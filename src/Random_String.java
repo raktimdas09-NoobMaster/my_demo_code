03ccc24880741b1c53074cbf8e3a865d7e67d71c
raktim <raktimdas09@gmail.com>
2025-08-14 23:17:20 +0530

    docker new sum

--- src/Random_String.java
+++ src/Random_String.java
@@ -5,7 +5,6 @@ public class Random_String {
     public static void main(String[] args) {
         Random random = new Random();
 
-        // Array of possible choices
         String[] choices = {"Rock", "Paper", "Scissors", "Mobile", "Notepad"};
 
         // Generate a random index between 0 and 4 (for 5 items in the array)
